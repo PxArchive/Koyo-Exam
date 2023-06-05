bd7e9a35abc3d80062750b526e17bdf35aa056d9
PxArchive <peterriemerbjorn@gmail.com>
2023-06-05 13:46:32 +0200

    Images Added

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function showSlides() {
 /* ------------------- Countdown Code -------------------*/
 
 // Set the date we're counting down to
-var countDownDate = new Date("Sep 8, 2023 18:00:00").getTime();
+var countDownDate = new Date("Nov 8, 2023 18:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
